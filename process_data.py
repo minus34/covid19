a51d2da05aa74b9e0b5574c7d5b2c639838fc00b
Hugh Saalmans <hugh.saalmans@gmail.com>
2020-03-12 18:33:31 +1100

    CSV file parsing into dicts - working

--- process_data.py
+++ process_data.py
@@ -1,6 +1,7 @@
 
 import csv
 
+from datetime import datetime
 from get_data import get_data
 
 # get_data()
@@ -11,39 +12,44 @@ files = [
     "time_series_19-covid-Recovered.csv"
 ]
 
+dict_list = list()
+
 for filename in files:
+    print("parsing {}".format(filename))
 
     with open(filename, "r") as f:
         reader = csv.reader(f, delimiter=',')
 
         i = 0
-        num_columns = 0
-
         dates = list()
-        countries = list()
-        states = list()
-        lats = list()
-        longs = list()
-        values = list()
 
         # parse data into lists
         for row in reader:
             if i == 0:
-                num_columns = len(row)
-
                 # get date list from column names
                 dates = row[4:]
             else:
-                states.append(row[0])
-                countries.append(row[1])
-                lats.append(row[2])
-                longs.append(row[3])
-                values.append(row[4:])
+                # get values for each day
+                values = row[4:]
 
+                j = 0
+
+                for date in dates:
+                    row_dict = dict()
+
+                    row_dict["province_state"] = row[0]
+                    row_dict["country_region"] = row[1]
+                    row_dict["latitude"] = float(row[2])
+                    row_dict["longitude"] = float(row[3])
+
+                    # convert date to standard format
+                    the_date = datetime.strptime(date, '%m/%d/%y')
+
+                    row_dict["the_date"] = the_date
+                    row_dict["persons"] = int(values[j])
+
+                    dict_list.append(row_dict)
+
+                    j += 1
             i += 1
 
-        print(values)
-
-    # f = open(filename, 'r')
-    # header_row = f.readline()
-    # print(header_row)
