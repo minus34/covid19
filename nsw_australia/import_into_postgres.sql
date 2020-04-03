0df4c75be68886276647988a4b84b3918add79df
Hugh Saalmans <hugh.saalmans@gmail.com>
2020-04-04 10:56:36 +1100

    working on dot density map

--- nsw_australia/import_into_postgres.sql
+++ nsw_australia/import_into_postgres.sql
@@ -17,6 +17,8 @@ ALTER TABLE covid19.raw_nsw_cases OWNER to postgres;
 COPY covid19.raw_nsw_cases
 FROM '/Users/hugh.saalmans/git/minus34/covid19/nsw_australia/input_files/nsw-covid19-data.csv' WITH (HEADER, DELIMITER ',', FORMAT CSV);
 
+delete from covid19.raw_nsw_cases where postcode is null;
+
 ANALYSE covid19.raw_nsw_cases;
 
 
@@ -29,9 +31,9 @@ SELECT notification_date,
        lhd_2010_name,
        lga_code19,
        lga_name19,
-       count(*) as cases,
-       null::integer as pop_2016,
-       null::geometry(multipolygon, 4283) as geom
+       count(*) as cases
+--       null::integer as pop_2016,
+--       null::geometry(multipolygon, 4283) as geom
 FROM covid19.raw_nsw_cases
 GROUP BY notification_date,
          postcode,
@@ -41,8 +43,35 @@ GROUP BY notification_date,
          lga_name19;
 ALTER TABLE covid19.nsw_cases OWNER to postgres;
 
-delete from covid19.nsw_cases where postcode is null;
-
 ANALYSE covid19.nsw_cases;
 
 ALTER TABLE covid19.nsw_cases ADD CONSTRAINT nsw_cases_pkey PRIMARY KEY (notification_date, postcode);
+
+
+-- get one person point per case
+DROP TABLE IF EXISTS covid19.nsw_cases_points;
+CREATE TABLE covid19.nsw_cases_points AS
+WITH adr AS (
+SELECT nsw.*,
+       pop.geom
+from covid19.nsw_cases as nsw
+inner join testing.address_principals_persons as pop on nsw.postcode = pop.postcode
+), row_nums as (
+ SELECT *, row_number() OVER (PARTITION BY postcode ORDER BY random()) as row_num
+ FROM adr
+)
+SELECT *
+FROM row_nums
+WHERE row_num <= cases
+ORDER BY postcode,
+      row_num
+;
+
+ANALYSE covid19.nsw_cases_points;
+
+
+
+select *
+from covid19.nsw_cases as nsw
+inner join testing.address_principals_persons as pop on nsw.postcode = pop.postcode;
+
